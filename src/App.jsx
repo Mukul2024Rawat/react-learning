2b83603b40064843ea7de6dd80b31a569bda35f6
Mukul Rawat <mukulsinghrawat819@gmail.com>
2024-06-23 18:22:26 +0530

    illustrate the concept of map method and imported Card component

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Card from "./components/Card";
+
+function App() {
+  const data = ["ajay", "rahul", "sachin", "udesh"];
+
+  // const data = [];
+
+  return (
+    <div>
+      {/* {data.map((item, index) => (
+        <h1 key={index}>{item}</h1>
+      ))} */}
+
+      {/* {data.map((item, index) => (
+        <div key={index} className="px-3 py-4 w-fit bg-zinc-300 rounded-md">
+          {item}
+        </div>
+      ))} */}
+
+      <Card />
+    </div>
+  );
+}
+
+export default App;
