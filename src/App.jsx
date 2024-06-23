d733d420e7da1c0ec909754b5c87bff760b6760c
Mukul Rawat <mukulsinghrawat819@gmail.com>
2024-06-23 18:12:05 +0530

    imported the Card compnent

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Amazon</h1>
+      <Card />
+    </div>
+  )
+}
+
+export default App
